34e02a765f6d0d9d74de9b6da547fdd1fc1fff9c
Simon <sraveh@users.noreply.github.com>
2016-08-24 14:30:04 -0700

    update 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	
 	public static void main(String[]args){
-		System.out.println("Hello world " + "Simon and David" );
+		System.out.println("Hello world " + "Simon and David!!!" );
 	}
 }
 
